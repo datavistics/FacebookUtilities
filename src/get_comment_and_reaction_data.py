4d537e54a7e20d15c5d41eec9fd6dd42b94c7aea
datavistics <datavistics@gmail.com>
2018-01-14 22:54:14 +0400

    Fixed url to be singular, updating rest of code to accomodate.

--- src/get_comment_and_reaction_data.py
+++ src/get_comment_and_reaction_data.py
@@ -4,16 +4,17 @@ import os
 import time
 
 import requests
+from more_itertools import chunked
 
 from user_data import user_data
 
 project_dir = os.path.join(os.getcwd(), os.pardir)
 output_dir = os.path.join(project_dir, 'output')
 access_token = user_data['token']
+ftype = ['comment', 'reaction']
 
 
 def convert_list(l):
-    ftype = ['comment', 'like', 'love', 'haha', 'sad', 'wow', 'angry']
     out = {}
     for ckey in l[0]:
         out[ckey] = {ftype[0]: l[0][ckey]['summary']['total_count']}
@@ -27,17 +28,13 @@ def convert_list(l):
 
 
 def list_to_facebook_data(list_post_ids):
-    # comment_url = f"https://graph.facebook.com/v2.11/{pid}/comments?summary=total_count&limit=0&access_token={access_token}"
-    batch_comment_url = f"https://graph.facebook.com/v2.11/comments?ids={list_post_ids}&summary=total_count&filter=stream&limit=0&access_token={access_token}"
-    batch_like_url = f"https://graph.facebook.com/v2.11/?ids={list_post_ids}&fields=reactions.type(LIKE).summary(total_count).limit(0)&access_token={access_token}"
-    batch_love_url = f"https://graph.facebook.com/v2.11/?ids={list_post_ids}&fields=reactions.type(LOVE).summary(total_count).limit(0)&access_token={access_token}"
-    batch_haha_url = f"https://graph.facebook.com/v2.11/?ids={list_post_ids}&fields=reactions.type(HAHA).summary(total_count).limit(0)&access_token={access_token}"
-    batch_sad_url = f"https://graph.facebook.com/v2.11/?ids={list_post_ids}&fields=reactions.type(SAD).summary(total_count).limit(0)&access_token={access_token}"
-    batch_wow_url = f"https://graph.facebook.com/v2.11/?ids={list_post_ids}&fields=reactions.type(WOW).summary(total_count).limit(0)&access_token={access_token}"
-    batch_angry_url = f"https://graph.facebook.com/v2.11/?ids={list_post_ids}&fields=reactions.type(ANGRY).summary(total_count).limit(0)&access_token={access_token}"
-    req_list = [requests.get(url) for url in (
-    batch_comment_url, batch_like_url, batch_love_url, batch_haha_url, batch_sad_url, batch_wow_url, batch_angry_url)]
-    return req_list
+    c2rmv = str.maketrans("", "", "'[] ")
+    fixed_post_ids = str(list_post_ids).translate(c2rmv)
+    batch_url = f"https://graph.facebook.com/v2.11/comments?ids={fixed_post_ids}" + \
+                '&fields=comments.summary(1)%7Bid%7D%2Creactions.summary(1)%7Bid%7D&' + \
+                f'access_token={access_token}'
+    print(batch_url)
+    return requests.get(batch_url)
 
 
 for file in os.listdir(os.path.join(output_dir, 'posts')):
@@ -45,7 +42,7 @@ for file in os.listdir(os.path.join(output_dir, 'posts')):
         continue
 
     file_date = datetime.date(*[int(x) for x in file.replace('.json', '').split('_')])
-    if file_date < datetime.date(2015, 8, 7):
+    if file_date < datetime.date(2017, 10, 1):
         continue
 
     file_path = os.path.join(output_dir, 'posts', file)
@@ -59,13 +56,10 @@ for file in os.listdir(os.path.join(output_dir, 'posts')):
             for post_index, post in enumerate(post_group):
                 post_id = post['id']
                 post_ids_by_group.append(post_id)
-                if post_index % 25 == 24:
-                    post_jsons = list_to_facebook_data(post_ids_by_group)
-                    day_list.append(json.dumps([json.loads(r.text) for r in post_jsons]))
-                    post_ids_by_group = []
 
-            post_jsons = list_to_facebook_data(post_ids_by_group)
-            day_list.append(json.dumps([json.loads(r.text) for r in post_jsons]))
+            for id_chunk in chunked(post_ids_by_group, 25):
+                post_jsons = list_to_facebook_data(id_chunk)
+                # day_list.append([{ft: json.loads(r.text)} for ft, r in zip(ftype, post_jsons)])
 
     with open(os.path.join(output_dir, 'comments', 'com_' + file), "w", encoding='utf-8') as myfile:
         myfile.write(json.dumps(day_list))
