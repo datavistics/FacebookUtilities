f1d28853d1cf6edb9484e5d9b9de8c5465cb7afe
datavistics <datavistics@gmail.com>
2018-01-15 14:33:35 +0400

    Major update: Will now functionally get facebook data by id and write to file in a readable json format.

--- src/get_comment_and_reaction_data.py
+++ src/get_comment_and_reaction_data.py
@@ -14,26 +14,21 @@ access_token = user_data['token']
 ftype = ['comment', 'reaction']
 
 
-def convert_list(l):
-    out = {}
-    for ckey in l[0]:
-        out[ckey] = {ftype[0]: l[0][ckey]['summary']['total_count']}
-        break
-    for ind, d in enumerate(l[1:]):
-        if not d:
-            continue
-        for k in d:
-            out[k][ftype[ind]] = d[k]['reactions']['summary']['total_count']
-    return d
+def convert_dict(indict):
+    outlist = {}
+    for post_id_l, post_data in indict.items():
+        num_c = post_data['comments']['summary']['total_count']
+        num_r = post_data['reactions']['summary']['total_count']
+        outlist[post_id_l] = {'num_comments': num_c, 'num_reactions': num_r}
+    return outlist
 
 
 def list_to_facebook_data(list_post_ids):
     c2rmv = str.maketrans("", "", "'[] ")
     fixed_post_ids = str(list_post_ids).translate(c2rmv)
-    batch_url = f"https://graph.facebook.com/v2.11/comments?ids={fixed_post_ids}" + \
+    batch_url = f"https://graph.facebook.com/v2.11/?ids={fixed_post_ids}" + \
                 '&fields=comments.summary(1)%7Bid%7D%2Creactions.summary(1)%7Bid%7D&' + \
                 f'access_token={access_token}'
-    print(batch_url)
     return requests.get(batch_url)
 
 
@@ -42,12 +37,12 @@ for file in os.listdir(os.path.join(output_dir, 'posts')):
         continue
 
     file_date = datetime.date(*[int(x) for x in file.replace('.json', '').split('_')])
-    if file_date < datetime.date(2017, 10, 1):
+    if file_date < datetime.date(2016, 12, 31):
         continue
 
     file_path = os.path.join(output_dir, 'posts', file)
 
-    day_list = []
+    day_dict = {}
     with open(file_path, encoding='utf-8') as json_data:
         d = json.load(json_data)
         start = time.time()
@@ -57,12 +52,11 @@ for file in os.listdir(os.path.join(output_dir, 'posts')):
                 post_id = post['id']
                 post_ids_by_group.append(post_id)
 
-            for id_chunk in chunked(post_ids_by_group, 25):
+            for id_chunk in chunked(post_ids_by_group, 50):
                 post_jsons = list_to_facebook_data(id_chunk)
-                # day_list.append([{ft: json.loads(r.text)} for ft, r in zip(ftype, post_jsons)])
+                post_json_dict = post_jsons.json()
+                day_dict.update(convert_dict(post_json_dict))
 
-    with open(os.path.join(output_dir, 'comments', 'com_' + file), "w", encoding='utf-8') as myfile:
-        myfile.write(json.dumps(day_list))
+    with open(os.path.join(output_dir, 'summary', 'summary_' + file), "w", encoding='utf-8') as myfile:
+        myfile.write(json.dumps(day_dict))
     print(f'{file}: {time.time() - start}')
-
-    # 1728661083894539/?fields=comments.summary(total_count).limit(0),reactions.type(LIKE).limit(0).summary(1),reactions.type(LOVE).limit(0).summary(1)
