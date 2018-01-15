b9445b7212675ef02d0c6b32a6b3a8d1ca1f4d6e
datavistics <datavistics@gmail.com>
2018-01-15 23:47:25 +0400

    Major update: added GUI functionality!

--- src/download_query.py
+++ src/download_query.py
@@ -7,10 +7,9 @@ import time
 
 import requests
 from facepy import GraphAPI
+from gooey import Gooey, GooeyParser
 
-from user_data import user_data
-
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(filename='download_query.log', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 project_dir = os.path.join(os.getcwd(), os.pardir)
@@ -27,7 +26,6 @@ def get_group_info(d_start, d_end, token, query, output_dir):
     :return:
     """
     graph = GraphAPI(token)
-
     # Create a pair of since and until days for the user defined range
     days = [(d_end - datetime.timedelta(days=delta)) for delta in
             range((d_end - d_start + datetime.timedelta(days=1)).days)]
@@ -45,6 +43,8 @@ def get_group_info(d_start, d_end, token, query, output_dir):
     # Calculate some metrics to make sure its working
     beg_time = time.time()
     total_posts = 0
+
+    # Clean up query
 
     for pair in day_pairs:
         num_posts = 0
@@ -75,19 +75,54 @@ def get_group_info(d_start, d_end, token, query, output_dir):
                     f'Total time: {time.time()-beg_time}\tTotal posts: {total_posts}')
 
 
+@Gooey(program_name='Facebook Query to json',
+       image_dir=os.path.dirname(os.path.realpath(__file__)),
+       required_cols=1,
+       optional_cols=1)
+def parse_args():
+    stored_args = {}
+
+    # Get filename info
+    script_name = os.path.splitext(os.path.basename(__file__))[0]
+    args_file = "{}-args.json".format(script_name)
+
+    # Uses previous args as defaults
+    if os.path.isfile(args_file):
+        with open(args_file) as data_file:
+            stored_args = json.load(data_file)
+
+    parser = GooeyParser(
+        description='This program will store facebook data in json format in the output directory.\nThe data will '
+                    'automatically be paged (beyond the 25 post limit), and downloaded/sorted by date.\n'
+                    'Posts have been tested, but feel free to try any data')
+    parser.add_argument('-s', '--d_start', action='store',
+                        widget='DateChooser', default=stored_args.get('d_start'), help='YYYY-MM-DD')
+    parser.add_argument('-e', '--d_end', action='store',
+                        widget='DateChooser',
+                        default=stored_args.get('d_end'),
+                        help='YYYY-MM-DD')
+    parser.add_argument('-o', '--output_dir', action='store',
+                        widget='DirChooser',
+                        default=stored_args.get('output_dir'),
+                        help='Leave blank for default')
+    parser.add_argument('-q', '--query', action='store', default=stored_args.get('query'),
+                        help="Full query for facebook, eg:\n'me?fields=id,name,picture'")
+    args = parser.parse_args()
+    with open(args_file, 'w') as data_file:
+        # Using vars(args) returns the data as a dictionary
+        json.dump(vars(args), data_file)
+    return args
+
+
 if __name__ == '__main__':
+    args = parse_args()
+    d_start = datetime.datetime.strptime(args.d_start, '%Y-%m-%d').date()
+    d_end = datetime.datetime.strptime(args.d_end, '%Y-%m-%d').date()
+    query = args.query
+    output_dir = args.output_dir
 
-    # get dates
-    # d_end = datetime.date(2017, 11, 12)
-    # d_start = datetime.date(2017, 11, 11)
+    with open('user_data.json') as json_file:
+        token_dict = json.load(json_file)
 
-    d_start = user_data['d_start']
-    d_end = user_data['d_end']
-    token = user_data['token']
-    query = user_data['query']
-    output_dir = user_data['output_dir']
-
-    if not output_dir:
-        output_dir = os.path.join(project_dir, 'output')
-
+    token = token_dict['token']
     get_group_info(d_start, d_end, token, query, output_dir)
